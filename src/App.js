a9d17fe784be130358e6c11d557d323c0ee09f2d
mohammedashiqs <mrashiq.tvm@gmail.com>
2024-08-14 17:11:36 +0530

    Components and Router-DOM completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Header from './components/layouts/Header';
+import Footer from './components/layouts/Footer';
+import Music from './components/music/Music';
+import Pnf from './components/layouts/Pnf';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Routes path={`/`} element={<Music/>}/>
+        <Routes path={`/music`} element={<Music/>}/>
+        <Routes path={`/*`} element={<Pnf/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/layouts/Header.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+function Header() {
+  return (
+<nav className='navbar navbar-expand-lg bg-primary navbar-dark'>
+   <div className='container'>
+        <NavLink to={'/'} className={'navbar-brand'}>MUSIC PLAYER</NavLink>
+
+        <button type='button' className='navbar-toggler' data-bs-target='#menu' data-bs-toggle='collapse'></button>
+            <span className='collapse navbar-collapse' id='menu'>
+                
+            </span>
+            <div className='collapse navbar-collapse' id='menu'>
+                <ul className='navbar-nav ms-auto'>
+                    <li className='nav-item'>
+                        <NavLink
+                        to={`/music`}
+                        className={({isActive}) => (isActive ? 'active nav-link' :
+                            'inactive nav-link')}>
+                                Music
+
+                        </NavLink>
+                    </li>
+                    <li className='nav-item'>
+                        <NavLink
+                        to={`/contact`}
+                        className={({isActive}) => (isActive ? 'active nav-link' :
+                            'inactive nav-link')}>
+                                Contact
+                        </NavLink>
+                    </li>
+                </ul>
+            </div>
+   </div>
+</nav>
+  )
+}
+
+export default Header
